a52e36be8b87b29d6aa604abb5271b2e01499768
Rene Armida <rene@armida.us>
2026-06-01 18:59:07 -0700

    move code to models module

--- kspoverlay.py
+++ kspoverlay.py
@@ -1,47 +1,8 @@
 from flask import Flask, render_template, redirect, request
+from models import *
 
 app = Flask(__name__)
 
-class KTimeParts:
-    HOURS_PER_DAY = 6
-    DAYS_PER_YEAR = 426
-    SECS_PER_DAY = 60*60*HOURS_PER_DAY
-    SECS_PER_YEAR = 60*60*HOURS_PER_DAY*DAYS_PER_YEAR
-
-    def __init__(self, timestamp):
-        self.year = (timestamp // self.SECS_PER_YEAR)
-        leftover = timestamp % self.SECS_PER_YEAR
-
-        self.day = leftover // self.SECS_PER_DAY
-        leftover = leftover % self.SECS_PER_DAY
-
-        self.hour = leftover // (60 * 60)
-        leftover = leftover % (60 * 60)
-
-        self.minute = leftover // 60
-
-        self.second = leftover % 60
-
-class KTimestamp:
-    def __init__(self, time):
-        self.time = time
-
-    def as_datetime(self, separator=' '):
-        timeparts = KTimeParts(self.time)
-        timeparts.year += 1 # year starts at 1
-        timeparts.day += 1 # same for day
-        return f'Y{timeparts.year}D{timeparts.day:03}{separator}{timeparts.hour:01}:{timeparts.minute:02}:{timeparts.second:02}'
-
-    def __sub(self, other):
-        return KTimestamp(self.time - other.time)
-
-    def as_interval(self, separator=' '):
-        timeparts = KTimeParts(self.time)
-        if timeparts.year > 0:
-            return f'{timeparts.year}Y{separator}{timeparts.day}D{separator}{timeparts.hour:01}H'
-        if timeparts.day > 0:
-            return f'{timeparts.day}D{separator}{timeparts.hour:01}:{timeparts.minute:02}:{timeparts.second:02}'
-        return f'{timeparts.hour:01}:{timeparts.minute:02}:{timeparts.second:02}'
 
 @app.template_filter()
 def ktime(tval):
